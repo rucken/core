f5d3c64bd7f6a97544975846f691bf7fb6738fba
endy <admin@site15.ru>
2017-03-05 20:21:16 +0500

    try fix build error

--- demo/src/environments/environment.prod.ts
+++ demo/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://58b570c93b33631200bf2d3d.mockapi.io/api',
-  accountLoginAction: '-login',
-  accountInfoAction: '-info'
+  apiUrl: 'http://localhost:5000/api',
+  accountLoginAction: '/login',
+  accountInfoAction: '/info'
 };
