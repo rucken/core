d180a6e1e3e58b6d811544a9830978894059db7c
endy <admin@site15.ru>
2017-03-25 20:31:17 +0500

    Fix error on select and add correct check response for items

--- src/shared/helpers/endpoint.helper.ts
+++ src/shared/helpers/endpoint.helper.ts
@@ -32,15 +32,21 @@ export class EndpointHelper {
     return this.httpHelper.post(this.actionUrl(endpointService, action), data);
   };
   actionResponse(endpointService: any, action?: any, response?: Response) {
+    let data: any;
+    if (response.json && _.isFunction(response.json)) {
+      data = response.json();
+    } else {
+      data = response;
+    }
     if (endpointService.name === 'account') {
       if (action === 'info' || action === 'login') {
-        if (response.json().token) {
-          localStorage.setItem('token', response.json().token);
+        if (data.token) {
+          localStorage.setItem('token', data.token);
         }
-        return new User(response.json().user);
+        return new User(data.user);
       }
       if (action === 'update') {
-        return new User(response.json());
+        return new User(data);
       }
     }
   };
--- src/shared/helpers/repository.helper.ts
+++ src/shared/helpers/repository.helper.ts
@@ -38,12 +38,23 @@ export class RepositoryHelper extends EndpointHelper {
     return apiUrl + `?${uri.toString()}`;
   };
   itemsResponse(repositoryService: any, response: any) {
-    let data = response.json();
+    let data: any;
+    if (response.json && _.isFunction(response.json)) {
+      data = response.json();
+    } else {
+      data = response;
+    }
     repositoryService.meta = new MetaModel(data['meta']);
     return data[_.camelCase(repositoryService.pluralName)];
   };
   itemResponse(repositoryService: any, response: any) {
-    return response.json()[_.camelCase(repositoryService.name)];
+    let data: any;
+    if (response.json && _.isFunction(response.json)) {
+      data = response.json();
+    } else {
+      data = response;
+    }
+    return data[_.camelCase(repositoryService.name)];
   };
   readItemRequest(repositoryService: any, key: any): Observable<Response> {
     return this.httpHelper.get(
