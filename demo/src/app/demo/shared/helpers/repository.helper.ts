2350945fbce0b82d5f66800b63b9e530f5e863fd
endy <admin@site15.ru>
2017-04-05 16:53:29 +0500

    Fix work with enter on grids

--- demo/src/app/demo/shared/helpers/repository.helper.ts
+++ demo/src/app/demo/shared/helpers/repository.helper.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Http, Headers, RequestOptions, Response, URLSearchParams } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import { AuthHttp, AuthConfig } from 'angular2-jwt';
-import { MetaModel, EndpointHelper, RepositoryHelper, HttpHelper } from '../../../../../../dist';
+import { MetaModel, EndpointHelper, RepositoryHelper, HttpHelper } from '../../../../../../src';
 import * as _ from 'lodash';
 import { environment } from '../../../../environments/environment';
 
