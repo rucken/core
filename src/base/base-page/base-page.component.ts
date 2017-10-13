b18bc13408e2bae56559698c8d18cdaa708203b6
endy <admin@site15.ru>
2017-10-13 14:19:08 +0500

    feat(base-page): Move load child routes to getter and setter

--- src/base/base-page/base-page.component.ts
+++ src/base/base-page/base-page.component.ts
@@ -18,26 +18,9 @@ export class BasePageComponent extends BaseComponent {
 
   title?: string;
   searchTextValue = '';
-  get childrenRoutes() {
-    const items: any[] = (
-      this.activatedRoute.snapshot.data && this.activatedRoute.snapshot.data.children ?
-        this.activatedRoute.snapshot.data.children : []).filter(
-      (item: any) =>
-        item.data &&
-        item.data.visible &&
-        this.account &&
-        this.account.checkPermissions([`read_${item.data.name}-frame`]) &&
-        this.checkWordInSearchText(item.data.title)
-      ).map(
-      (item: any) => {
-        const newItem = item.data;
-        newItem.url = `${this.name}/${newItem.name}`;
-        return newItem;
-      });
-    return _.sortBy(items, [
-      (item: any) => { return item.title }
-    ]);
-  }
+
+  private _childrenRoutes: any[] = [];
+
   get account(): any | User {
     return this.accountService.account;
   }
@@ -51,9 +34,12 @@ export class BasePageComponent extends BaseComponent {
     public sharedService: SharedService
   ) {
     super();
-    sharedService.linkTranslateService();
-    translateService.onLangChange.subscribe(() => this.init());
-    accountService.account$.subscribe(() => this.init());
+    this.afterCreate();
+  }
+  afterCreate() {
+    this.sharedService.linkTranslateService();
+    this.translateService.onLangChange.subscribe(() => this.init());
+    this.accountService.account$.subscribe(() => this.init());
   }
   init() {
     super.init();
@@ -73,6 +59,35 @@ export class BasePageComponent extends BaseComponent {
     this.app.currentPageName = this.name;
     this.app.currentPageTitle = pageTitle;
     this.title = pageTitle;
+    this.initChildrenRoutes()
+  }
+  initChildrenRoutes() {
+    this.childrenRoutes = this.activatedRoute.snapshot.data && this.activatedRoute.snapshot.data.children ?
+      this.activatedRoute.snapshot.data.children : [];
+  }
+  sortChildrenRoutes(routes: any[]) {
+    return _.sortBy(routes, [
+      (route: any) => { return route.title }
+    ]);
+  }
+  set childrenRoutes(routes: any[]) {
+    this._childrenRoutes = routes;
+  }
+  get childrenRoutes() {
+    const items: any[] = this._childrenRoutes.filter(
+      (item: any) =>
+        item.data &&
+        item.data.visible &&
+        this.account &&
+        this.account.checkPermissions([`read_${item.data.name}-frame`]) &&
+        this.checkWordInSearchText(item.data.title)
+    ).map(
+      (item: any) => {
+        const newItem = item.data;
+        newItem.url = `${this.name}/${newItem.name}`;
+        return newItem;
+      });
+    return this.sortChildrenRoutes(items);
   }
   checkWordInSearchText(word: string) {
     return this.translateService.instant(word).toLowerCase().indexOf(this.searchTextValue.toLowerCase()) !== -1;
