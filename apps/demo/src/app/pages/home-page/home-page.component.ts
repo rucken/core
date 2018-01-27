ca10459fa6987f22faace705c807111345e40f65
endy <admin@site15.ru>
2018-01-27 14:11:05 +0500

    chore(docs): Update README.md

--- apps/demo/src/app/pages/home-page/home-page.component.ts
+++ apps/demo/src/app/pages/home-page/home-page.component.ts
@@ -1,16 +1,14 @@
 import { Component, Injector } from '@angular/core';
-import { BasePageComponent } from '@rucken/web';
+import { HomePageComponent } from '@rucken/web';
 
 @Component({
   selector: 'demo-home-page',
   templateUrl: './home-page.component.html'
 })
-export class DemoHomePageComponent extends BasePageComponent {
+export class DemoHomePageComponent extends HomePageComponent {
 
-  constructor(
-    public injector: Injector
-  ) {
-    super(injector);
+  afterCreate() {
+    super.afterCreate();
     const readme = require('html-loader!markdown-loader!./../../../../../../README.md');
     this.readme = readme.replace('<h1 id="rucken">rucken</h1>', '');
   }
