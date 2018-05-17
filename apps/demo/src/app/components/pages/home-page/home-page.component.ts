f99f2e20bc317de61baf64a2fd4d029ebaed4d55
endy <admin@site15.ru>
2018-05-17 10:04:28 +0500

    chore(demo): Fix path to README.md

--- apps/demo/src/app/components/pages/home-page/home-page.component.ts
+++ apps/demo/src/app/components/pages/home-page/home-page.component.ts
@@ -8,10 +8,8 @@ import { environment } from '../../../../environments/environment';
 })
 export class HomePageComponent {
 
-  readme = environment.type === 'backend' ?
-    require('html-loader!markdown-loader!./../../../../../../../../README.md') :
-    require('html-loader!markdown-loader!./../../../../../../../README.md')
-      .replace('<h1 id="todo">todo</h1>', '');
+  readme = require('html-loader!markdown-loader!./../../../../../../../README.md')
+    .replace('<h1 id="rucken">rucken</h1>', '');
 
   constructor(
     public activatedRoute: ActivatedRoute
