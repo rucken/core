e9a2db76cd4a5b25371494109d81973b4188f474
EndyKaufman <admin@site15.ru>
2018-09-18 19:00:34 +0500

    fix(refactor): Move titles from template to component Class #215

--- libs/rucken/web/src/lib/entities/group/groups-grid/groups-grid.component.ts
+++ libs/rucken/web/src/lib/entities/group/groups-grid/groups-grid.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, Inject, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Inject, Input, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { ErrorsExtractor, Group, GROUP_CONFIG_TOKEN, translate } from '@rucken/core';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
@@ -8,81 +8,83 @@ import { MessageModalService } from '../../../modals/message-modal/message-modal
 import { GroupModalComponent } from '../group-modal/group-modal.component';
 
 @Component({
-  selector: 'groups-grid',
-  templateUrl: './groups-grid.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush
+    selector: 'groups-grid',
+    templateUrl: './groups-grid.component.html',
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class GroupsGridComponent extends BaseEntityListComponent<Group>
-  implements OnInit {
-  constructor(
-    public modalService: BsModalService,
-    protected errorsExtractor: ErrorsExtractor,
-    protected translateService: TranslateService,
-    protected dynamicRepository: DynamicRepository,
-    protected messageModalService: MessageModalService,
-    @Inject(GROUP_CONFIG_TOKEN) protected groupsConfig: IRestProviderOptions<Group>
-  ) {
-    super(dynamicRepository.fork<Group>(Group), modalService, Group);
-  }
-  ngOnInit() {
-    if (!this.mockedItems) {
-      this.useRest({
-        apiUrl: this.apiUrl,
-        ...this.groupsConfig
-      });
+    implements OnInit {
+    @Input()
+    title = translate('Groups');
+    constructor(
+        public modalService: BsModalService,
+        protected errorsExtractor: ErrorsExtractor,
+        protected translateService: TranslateService,
+        protected dynamicRepository: DynamicRepository,
+        protected messageModalService: MessageModalService,
+        @Inject(GROUP_CONFIG_TOKEN) protected groupsConfig: IRestProviderOptions<Group>
+    ) {
+        super(dynamicRepository.fork<Group>(Group), modalService, Group);
     }
-    if (this.mockedItems) {
-      this.useMock({
-        items: this.mockedItems,
-        ...this.groupsConfig
-      });
+    ngOnInit() {
+        if (!this.mockedItems) {
+            this.useRest({
+                apiUrl: this.apiUrl,
+                ...this.groupsConfig
+            });
+        }
+        if (this.mockedItems) {
+            this.useMock({
+                items: this.mockedItems,
+                ...this.groupsConfig
+            });
+        }
     }
-  }
-  createDeleteModal(item: Group): BsModalRef {
-    return this.modalService.show(GroupModalComponent, {
-      class: 'modal-md',
-      initialState: {
-        title: this.strings.deleteTitle,
-        message: this.strings.deleteMessage,
-        yesTitle: translate('Delete'),
-        data: item,
-        apiUrl: this.apiUrl
-      }
-    });
-  }
-  createCreateModal(): BsModalRef {
-    const item = new Group();
-    return this.modalService.show(GroupModalComponent, {
-      class: 'modal-md',
-      initialState: {
-        title: this.strings.createTitle,
-        yesTitle: translate('Create'),
-        data: item,
-        apiUrl: this.apiUrl
-      }
-    });
-  }
-  createUpdateModal(item?: Group): BsModalRef {
-    return this.modalService.show(GroupModalComponent, {
-      class: 'modal-md',
-      initialState: {
-        title: this.strings.updateTitle,
-        yesTitle: translate('Save'),
-        data: item,
-        apiUrl: this.apiUrl
-      }
-    });
-  }
-  createViewModal(item?: Group): BsModalRef {
-    return this.modalService.show(GroupModalComponent, {
-      class: 'modal-md',
-      initialState: {
-        title: this.strings.viewTitle,
-        noTitle: translate('Close'),
-        readonly: true,
-        data: item,
-        apiUrl: this.apiUrl
-      }
-    });
-  }
+    createDeleteModal(item: Group): BsModalRef {
+        return this.modalService.show(GroupModalComponent, {
+            class: 'modal-md',
+            initialState: {
+                title: this.strings.deleteTitle,
+                message: this.strings.deleteMessage,
+                yesTitle: translate('Delete'),
+                data: item,
+                apiUrl: this.apiUrl
+            }
+        });
+    }
+    createCreateModal(): BsModalRef {
+        const item = new Group();
+        return this.modalService.show(GroupModalComponent, {
+            class: 'modal-md',
+            initialState: {
+                title: this.strings.createTitle,
+                yesTitle: translate('Create'),
+                data: item,
+                apiUrl: this.apiUrl
+            }
+        });
+    }
+    createUpdateModal(item?: Group): BsModalRef {
+        return this.modalService.show(GroupModalComponent, {
+            class: 'modal-md',
+            initialState: {
+                title: this.strings.updateTitle,
+                yesTitle: translate('Save'),
+                data: item,
+                apiUrl: this.apiUrl
+            }
+        });
+    }
+    createViewModal(item?: Group): BsModalRef {
+        return this.modalService.show(GroupModalComponent, {
+            class: 'modal-md',
+            initialState: {
+                title: this.strings.viewTitle,
+                noTitle: translate('Close'),
+                readonly: true,
+                data: item,
+                apiUrl: this.apiUrl
+            }
+        });
+    }
 }
--- libs/rucken/web/src/lib/entities/permission/permissions-grid/permissions-grid.component.ts
+++ libs/rucken/web/src/lib/entities/permission/permissions-grid/permissions-grid.component.ts
@@ -8,89 +8,91 @@ import { MessageModalService } from '../../../modals/message-modal/message-modal
 import { PermissionModalComponent } from '../permission-modal/permission-modal.component';
 
 @Component({
-  selector: 'permissions-grid',
-  templateUrl: './permissions-grid.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush
+    selector: 'permissions-grid',
+    templateUrl: './permissions-grid.component.html',
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PermissionsGridComponent
-  extends BaseEntityListComponent<Permission>
-  implements OnInit {
-  @Input()
-  apiUrl?: string;
+    extends BaseEntityListComponent<Permission>
+    implements OnInit {
+    @Input()
+    title = translate('Permissions');
+    @Input()
+    apiUrl?: string;
 
-  constructor(
-    public modalService: BsModalService,
-    protected errorsExtractor: ErrorsExtractor,
-    protected translateService: TranslateService,
-    protected dynamicRepository: DynamicRepository,
-    protected messageModalService: MessageModalService,
-    @Inject(PERMISSIONS_CONFIG_TOKEN) protected permissionsConfig: IRestProviderOptions<Permission>
-  ) {
-    super(
-      dynamicRepository.fork<Permission>(Permission),
-      modalService,
-      Permission
-    );
-  }
-  ngOnInit() {
-    if (!this.mockedItems) {
-      this.useRest({
-        apiUrl: this.apiUrl,
-        ...this.permissionsConfig
-      });
+    constructor(
+        public modalService: BsModalService,
+        protected errorsExtractor: ErrorsExtractor,
+        protected translateService: TranslateService,
+        protected dynamicRepository: DynamicRepository,
+        protected messageModalService: MessageModalService,
+        @Inject(PERMISSIONS_CONFIG_TOKEN) protected permissionsConfig: IRestProviderOptions<Permission>
+    ) {
+        super(
+            dynamicRepository.fork<Permission>(Permission),
+            modalService,
+            Permission
+        );
     }
-    if (this.mockedItems) {
-      this.useMock({
-        items: this.mockedItems,
-        ...this.permissionsConfig
-      });
+    ngOnInit() {
+        if (!this.mockedItems) {
+            this.useRest({
+                apiUrl: this.apiUrl,
+                ...this.permissionsConfig
+            });
+        }
+        if (this.mockedItems) {
+            this.useMock({
+                items: this.mockedItems,
+                ...this.permissionsConfig
+            });
+        }
     }
-  }
-  createDeleteModal(item: Permission): BsModalRef {
-    return this.modalService.show(PermissionModalComponent, {
-      class: 'modal-md',
-      initialState: {
-        title: this.strings.deleteTitle,
-        message: this.strings.deleteMessage,
-        yesTitle: translate('Delete'),
-        data: item,
-        apiUrl: this.apiUrl
-      }
-    });
-  }
-  createCreateModal(): BsModalRef {
-    const item = new Permission();
-    return this.modalService.show(PermissionModalComponent, {
-      class: 'modal-md',
-      initialState: {
-        title: this.strings.createTitle,
-        yesTitle: translate('Create'),
-        data: item,
-        apiUrl: this.apiUrl
-      }
-    });
-  }
-  createUpdateModal(item?: Permission): BsModalRef {
-    return this.modalService.show(PermissionModalComponent, {
-      class: 'modal-md',
-      initialState: {
-        title: this.strings.updateTitle,
-        yesTitle: translate('Save'),
-        data: item,
-        apiUrl: this.apiUrl
-      }
-    });
-  }
-  createViewModal(item?: Permission): BsModalRef {
-    return this.modalService.show(PermissionModalComponent, {
-      class: 'modal-md',
-      initialState: {
-        title: this.strings.viewTitle,
-        noTitle: translate('Close'),
-        readonly: true,
-        data: item,
-        apiUrl: this.apiUrl
-      }
-    });
-  }
+    createDeleteModal(item: Permission): BsModalRef {
+        return this.modalService.show(PermissionModalComponent, {
+            class: 'modal-md',
+            initialState: {
+                title: this.strings.deleteTitle,
+                message: this.strings.deleteMessage,
+                yesTitle: translate('Delete'),
+                data: item,
+                apiUrl: this.apiUrl
+            }
+        });
+    }
+    createCreateModal(): BsModalRef {
+        const item = new Permission();
+        return this.modalService.show(PermissionModalComponent, {
+            class: 'modal-md',
+            initialState: {
+                title: this.strings.createTitle,
+                yesTitle: translate('Create'),
+                data: item,
+                apiUrl: this.apiUrl
+            }
+        });
+    }
+    createUpdateModal(item?: Permission): BsModalRef {
+        return this.modalService.show(PermissionModalComponent, {
+            class: 'modal-md',
+            initialState: {
+                title: this.strings.updateTitle,
+                yesTitle: translate('Save'),
+                data: item,
+                apiUrl: this.apiUrl
+            }
+        });
+    }
+    createViewModal(item?: Permission): BsModalRef {
+        return this.modalService.show(PermissionModalComponent, {
+            class: 'modal-md',
+            initialState: {
+                title: this.strings.viewTitle,
+                noTitle: translate('Close'),
+                readonly: true,
+                data: item,
+                apiUrl: this.apiUrl
+            }
+        });
+    }
 }
