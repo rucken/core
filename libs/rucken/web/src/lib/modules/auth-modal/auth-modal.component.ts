5a71d392c826751191d3c226893c10670c5e2835
EndyKaufman <admin@site15.ru>
2019-02-06 13:45:00 +0500

    fix(web): Remove inputs and outputs properties in auth-modal, replace them to bindIO

--- libs/rucken/web/src/lib/modules/auth-modal/auth-modal.component.ts
+++ libs/rucken/web/src/lib/modules/auth-modal/auth-modal.component.ts
@@ -1,6 +1,7 @@
-import { ChangeDetectionStrategy, Component, Inject, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Inject, OnInit, ViewChild } from '@angular/core';
 import { AuthModalComponent, AUTH_MODAL_CONFIG_TOKEN, IAuthModalConfig } from '@rucken/core';
-import { BindIoInner } from 'ngx-bind-io';
+import { BindIoInner, NgxBindIoService } from 'ngx-bind-io';
+import { PromptFormModalComponent } from '../../components/prompt-form-modal/prompt-form-modal.component';
 
 @BindIoInner()
 @Component({
@@ -9,9 +10,22 @@ import { BindIoInner } from 'ngx-bind-io';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class WebAuthModalComponent extends AuthModalComponent implements OnInit {
+  @ViewChild('promptFormModal')
+  promptFormModal: PromptFormModalComponent;
   constructor(
-    @Inject(AUTH_MODAL_CONFIG_TOKEN) private _authModalConfig: IAuthModalConfig
+    @Inject(AUTH_MODAL_CONFIG_TOKEN) private _authModalConfig: IAuthModalConfig,
+    private _ngxBindIoService: NgxBindIoService,
+    private _changeDetectorRef: ChangeDetectorRef
   ) {
     super(_authModalConfig);
+  }
+  ngOnInit() {
+    super.ngOnInit();
+    this._ngxBindIoService.linkHostToInner(
+      this,
+      this.promptFormModal,
+      undefined,
+      this._changeDetectorRef
+    );
   }
 }
