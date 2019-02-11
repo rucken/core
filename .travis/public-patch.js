73be63f19638e70194bdf1ae177707e8b303819f
EndyKaufman <admin@site15.ru>
2019-02-11 21:50:32 +0500

    chore(ci): Update public-patch.js

--- .travis/public-patch.js
+++ .travis/public-patch.js
@@ -1,21 +1,23 @@
 const fs = require('fs');
-const f_gitignore = '.gitignore';
-const f_package_json = 'package.json';
+const f_gitignore = './.gitignore';
+const f_package_json = './package.json';
 
-var data = '';
+var data = '', obj = {}, txt = '';
 
 data = fs.readFileSync(f_gitignore, 'utf8');
-var result = data.replace(/\/dist/g, '/dist/rucken').replace(/server.js/g, '');
-fs.writeFileSync(f_gitignore, result, 'utf8');
+txt = data.replace(/\/dist/g, '/dist/rucken').replace(/server.js/g, '');
+fs.writeFileSync(f_gitignore, txt, 'utf8');
 
 console.log('Updated .gitignore');
+console.log(txt);
 
 data = fs.readFileSync(f_package_json, 'utf8');
-var result = JSON.parse(data);
-delete result['devDependencies'];
-result.start = 'node ./server.js';
-result.build = 'exit 0';
-result.test = 'exit 0';
-fs.writeFileSync(f_package_json, JSON.stringify(result), 'utf8');
+obj = JSON.parse(data);
+delete obj['devDependencies'];
+obj.start = 'node ./server.js';
+obj.build = 'exit 0';
+obj.test = 'exit 0';
+fs.writeFileSync(f_package_json, JSON.stringify(obj), 'utf8');
 
 console.log('Updated package.json');
+console.log(JSON.stringify(obj));
