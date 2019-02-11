cb1b9ddfee7fbb91bc2cd03c2978a14ab7dfec18
EndyKaufman <admin@site15.ru>
2019-02-11 21:24:57 +0500

    chore(ci): Update public-patch.js

--- .travis/public-patch.js
+++ .travis/public-patch.js
@@ -2,26 +2,16 @@ const fs = require('fs');
 const f_gitignore = '.gitignore';
 const f_package_json = 'package.json';
 
-fs.readFile(f_gitignore, 'utf8', function (err, data) {
-    if (err) {
-        return console.log(err);
-    }
-    var result = data.replace(/\/dist/g, '/dist/rucken').replace(/server.js/g, '');
-    fs.writeFile(f_gitignore, result, 'utf8', function (err) {
-        if (err) return console.log(err);
-    });
-});
+var data = '';
 
-fs.readFile(f_package_json, 'utf8', function (err, data) {
-    if (err) {
-        return console.log(err);
-    }
-    var result = JSON.parse(data);
-    delete result['devDependencies'];
-    result.start = 'node ./server.js';
-    result.build = 'exit 0';
-    result.test = 'exit 0';
-    fs.writeFile(f_package_json, JSON.stringify(result), 'utf8', function (err) {
-        if (err) return console.log(err);
-    });
-});
+data = fs.readFileSync(f_gitignore, 'utf8');
+var result = data.replace(/\/dist/g, '/dist/rucken').replace(/server.js/g, '');
+fs.writeFileSync(f_gitignore, result, 'utf8');
+
+data = fs.readFileSync(f_package_json, 'utf8');
+var result = JSON.parse(data);
+delete result['devDependencies'];
+result.start = 'node ./server.js';
+result.build = 'exit 0';
+result.test = 'exit 0';
+fs.writeFileSync(f_package_json, JSON.stringify(result), 'utf8');
