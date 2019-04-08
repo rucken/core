2dc68a0210e2533052842753dc10211382959548
EndyKaufman <admin@site15.ru>
2019-04-08 23:32:39 +0500

    fix: Downgrade version of ngx-bootstrap to 3.2.0 and freeze all dependencies

--- libs/rucken/web/src/lib/web.module.ts
+++ libs/rucken/web/src/lib/web.module.ts
@@ -2,15 +2,10 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { LANGUAGES_ITEM_CONFIG_TOKEN, translate } from '@rucken/core';
 import { BsLocaleService } from 'ngx-bootstrap';
-import { defineLocale } from 'ngx-bootstrap/chronos';
-import { enGbLocale, ruLocale } from 'ngx-bootstrap/locale';
 import { RuI18n } from '../i18n/ru.i18n';
 import { NavbarModule } from './components/navbar/navbar.module';
 import { WebAuthModalModule } from './modules/auth-modal/auth-modal.module';
 import { WebModalsModule } from './modules/modals/modals.module';
-
-defineLocale('ru', ruLocale);
-defineLocale('en', enGbLocale);
 
 @NgModule({
   imports: [CommonModule, WebAuthModalModule, WebModalsModule, NavbarModule],
